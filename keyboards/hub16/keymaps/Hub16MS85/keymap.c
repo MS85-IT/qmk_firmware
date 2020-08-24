7c88dfcbd357e7afc1b92029ccd8a412c87056fd
MS85-IT <66919531+MS85-IT@users.noreply.github.com>
2020-08-24 23:40:03 +0200

    Update keymap.c

--- keyboards/hub16/keymaps/Hub16MS85/keymap.c
+++ keyboards/hub16/keymaps/Hub16MS85/keymap.c
@@ -89,7 +89,7 @@
 	  	KC_S, KC_V, KC_1, KC_2,
 	  	KC_3, KC_4, KC_5, KC_6,
 	  	KC_7, KC_8, KC_9, KC_0,
-	  	TD(BASE), KC_COMMA, KC_DOT, KC_SLASH
+	  	TD(BASE), KC_COMMA, KC_EQUAL, KC_SLASH
 	  ),
 	};
 	
